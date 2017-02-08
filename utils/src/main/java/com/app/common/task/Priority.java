7e82cf212724c64c62bfba9d433deb7e72b5aff1
CodeGhost <279095547@qq.com>
2017-02-08 16:26:17 +0800

    删除目录utils

--- utils/src/main/java/com/app/common/task/Priority.java
+++
@@ -1,9 +0,0 @@
-package com.app.common.task;
-
-/**
- * Created by Haru on 2016/4/12 0012.
- * 任务的优先级
- */
-public enum  Priority {
-    UI_TOP, UI_NORMAL, UI_LOW, DEFAULT, BG_TOP, BG_NORMAL, BG_LOW;
-}
